15b2a95c42875007abfc4a9817cc224a9075f120
swaption2009 <swaption2009@gmail.com>
2017-06-03 16:36:21 -0700

    model generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603172219) do
+ActiveRecord::Schema.define(version: 20170603232432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170603172219) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.float "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
